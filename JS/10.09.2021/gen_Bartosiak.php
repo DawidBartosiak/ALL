eb002bf412fc983c74d935adfdf8533b30135255
Dawid Bartosiak <DawBar@Pracownia.com>
2021-10-11 14:34:43 +0200

    komentarz

---
+++ JS/10.09.2021/gen_Bartosiak.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <body>
+
+        <input type="submit" value="LOSUJ">
+        </form>
+
+        </body>
+    </head>
+</html>
+    
+<?php
+                
+    function LOSUJ($n)
