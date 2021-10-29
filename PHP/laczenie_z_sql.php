e77677faf99095df779a0a453a5e23a6721a58b5
DawidBartosiak <90040753+DawidBartosiak@users.noreply.github.com>
2021-10-29 13:43:36 +0200

    Update laczenie_z_sql.php

--- PHP/laczenie_z_sql.php
+++ PHP/laczenie_z_sql.php
@@ -1,18 +1,16 @@
 <?php
-
-$link = mysql_connect("localhost", "", "")
-or die("Could not select database");
-
-mysql_select_db("bazaosob")
-or die("Could not selected database");
-
-$query = "SELECT * FROM osoby";
-$result = mysql_query($query)
-or die("Failed");
-
-while($row = mysql_fetch_array($results))
-{
-    echo "<TD><TD>" . $row["imie"]
-    "</TR><TD>" . $row["nazwisko"]
-    "</TD></TR>";
-}
+    $link = mysqli_connect("localhost", "root", "","komis")
+    or die("Could not select database");
+    
+    mysqli_select_db($link,"komis")
+    or die("Could not selected database");
+    
+    $query = "SELECT * FROM samochody";
+    $result = mysqli_query($link,$query)
+    or die("Failed");
+    
+    while($row = mysqli_fetch_array($result))
+    {
+        echo $row["wykaz"];
+    }
+?>
